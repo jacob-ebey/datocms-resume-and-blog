34cc5c7343e367b14fc6759166623c2c53089690
Jacob Ebey <jacob.ebey@live.com>
2020-07-24 18:27:29 -0700

    Added responsive meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import 'isomorphic-unfetch'
+import Head from 'next/head'
 
 import { AbFlagsProvider } from '../contexts/ab-flags'
 import { getAbFlags } from '../lib/api'
@@ -6,9 +7,14 @@ import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AbFlagsProvider fetchFlags={getAbFlags}>
-      <Component {...pageProps} />
-    </AbFlagsProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <AbFlagsProvider fetchFlags={getAbFlags}>
+        <Component {...pageProps} />
+      </AbFlagsProvider>
+    </>
   )
 }
 
